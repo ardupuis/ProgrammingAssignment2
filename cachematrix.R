cce154d127b58a2b21cde28f0501c62390b71185
ardupuis <ardupuis@abilitech.net>
2014-06-21 09:47:57 -0400

    Modified by Andre Dupuis June 21, 2014 Part of Assignment 2 in the 'R Programming' course from Johns Hopkins

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Use:
+## a <- makeCacheMatrix(enter matrix here),
+## then use cacheSolve(a) to invert the matrix from cache if present,
+## live if not.
 
-## Write a short comment describing this function
+## The first function, makeVector creates a special "vector", 
+## which is really a list containing a function to
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the inverse matrix
+## 4. get the value of the inverse matrix
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)    
 }
 
 
-## Write a short comment describing this function
+## The following function solves the special "vector" 
+## created with the above function. However, it first checks to see if 
+## the inverted matrix is already present in cache. If so, it gets the 
+## inverted matrix from the cache and skips the solve function. Otherwise, 
+## it inverts the matrix and sets the value of the matrix in the cache via 
+## the setinverse function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting inverted matrix")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
